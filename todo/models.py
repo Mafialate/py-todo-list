005ec9116503cfd15a69e759395a376e2926d5a3
Mafialate <mafialate@ukr.net>
2026-05-02 14:53:05 +0200

    Implement task and tag models & Fill in requirements.txt

---
+++ todo/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Task(models.Model):
+    IS_DONE_CHOICES = [
+        (True, 'Done'),
+        (False, 'Not done'),
+    ]
+
+    name = models.CharField(max_length=255)
+    content = models.TextField()
+    datetime = models.DateTimeField(auto_now_add=True)
+    deadline = models.DateTimeField(null=True, blank=True)
+    is_done = models.BooleanField(choices=IS_DONE_CHOICES, default=False)
+    tags = models.ManyToManyField(Tag, blank=True, related_name='tasks')
+
+    def __str__(self):
+        return self.name
