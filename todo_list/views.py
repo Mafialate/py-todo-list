f3d594a3c00fd544e3ee3301fe3ed084d855ae9d
Mafialate <mafialate@ukr.net>
2026-05-04 20:30:11 +0200

    Add views

--- todo_list/views.py
+++ todo_list/views.py
@@ -1,3 +1,64 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect
+from django.urls import reverse_lazy
+from django.views import View
+from django.views.generic import (
+    ListView,
+    CreateView,
+    UpdateView,
+    DeleteView
+)
 
-# Create your views here.
+from todo_list.models import Task, Tag
+
+
+class TaskListView(ListView):
+    model = Task
+    queryset = Task.objects.all().prefetch_related("tags")
+
+
+class TaskToggleStatusView(View):
+    def post(self, request, pk):
+        task = get_object_or_404(Task, pk=pk)
+        task.is_done = not task.is_done
+        task.save()
+
+        return redirect("todo_list:task-list")
+
+
+class TaskCreateView(CreateView):
+    model = Task
+    fields = "__all__"
+    success_url = reverse_lazy("todo_list:task-list")
+
+
+class TaskUpdateView(UpdateView):
+    model = Task
+    fields = "__all__"
+    success_url = reverse_lazy("todo_list:task-list")
+
+
+class TaskDeleteView(DeleteView):
+    model = Task
+    success_url = reverse_lazy("todo_list:task-list")
+
+#---------------------------------#
+
+class TagListView(ListView):
+    model = Tag
+
+
+class TagCreateView(CreateView):
+    model = Tag
+    fields = "__all__"
+    success_url = reverse_lazy("todo_list:tag-list")
+
+
+class TagUpdateView(UpdateView):
+    model = Tag
+    fields = "__all__"
+    success_url = reverse_lazy("todo_list:tag-list")
+
+
+class TagDeleteView(DeleteView):
+    model = Tag
+    success_url = reverse_lazy("todo_list:tag-list")
